2030823f94bd6e1ac262f2dca19d7ec61f8a60e2
Nagy Ádám <nagy@geox.hu>
2013-05-27 16:01:54 +0200

    minimal code refactor and javadoc

--- src/main/resources/META-INF/resources/firstswim/javascript/controller/frame/left/dictionary/Dictionary.js
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/frame/left/dictionary/Dictionary.js
@@ -15,8 +15,13 @@ enyo.kind({
         { tag: 'div', name: 'list' }
     ],
 
+    /**
+     * The create function set the css classes, the contents and create an entity
+     * list into the dictonary.
+     */
     create: function(){
         this.inherited(arguments);
+        this.$.nameDiv.setClasses(this.nameClass);
         this.$.nameDiv.setContent(this.dictionaryName);
         for(var i=0;i<this.entityList.length;++i){
             var unchecked = this.isUncheckedEntity(this.entityList[i]);
@@ -33,10 +38,21 @@ enyo.kind({
         }
     },
 
+    /**
+     * After the user over the mouse an entity, the program gets the details about
+     * the chosen entity, and this function is called for update the details box.
+     * @param details the details object, which contains information about the entity
+     */
     updateDetails: function(details){
         this.owner.owner[this.showDetailsFunction](details);
     },
 
+    /**
+     * This function decide that an entity is unchecked or not (the unchecked
+     * list contains the entity or not).
+     * @param entity the checked entity
+     * @return true, if it is unchecked, false otherwise
+     */
     isUncheckedEntity: function(entity){
         if(this.uncheckedEntities.indexOf(entity) !== -1){
             return true;
@@ -44,6 +60,12 @@ enyo.kind({
         return false;
     },
 
+    /**
+     * This function is called when the user checked/unchecked one entity.
+     * It handles the unchecked entity list and call the parent filter function.
+     * @param entity the chosen entity
+     * @param checked the entity is checked or not
+     */
     updateEntities: function(entity, checked){
         if(checked){
             // Remove element
@@ -54,10 +76,6 @@ enyo.kind({
             this.uncheckedEntities.push(entity);
         }
         this.owner.owner.filter();
-    },
-
-    rendered: function(){
-        this.$.nameDiv.setClasses(this.nameClass);
     }
 
 });
