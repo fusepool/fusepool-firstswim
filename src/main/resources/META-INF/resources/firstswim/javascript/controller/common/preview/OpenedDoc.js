909b5d15ea449c6c272fd8ae8f5d1a616f757505
Nagy Ádám <nagy@geox.hu>
2013-07-12 11:53:43 +0200

    templates in other file (test)

--- src/main/resources/META-INF/resources/firstswim/javascript/controller/common/preview/OpenedDoc.js
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/common/preview/OpenedDoc.js
@@ -33,6 +33,9 @@ enyo.kind(
         this.$.content.setClasses(this.documentContentClass);
         this.$.loader.setClasses(this.loaderClass);
         this.$.scroller.setClasses(this.openedDocScrollerClass);
+
+        // load the preview templates
+        jQuery("#templates").load("templates.html");
     },
 
     components: [
