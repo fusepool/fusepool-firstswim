87763c34b4395ce7020af5e169db6de290e41f4a
Nagy Ádám <nagy@geox.hu>
2013-09-24 12:44:23 +0200

    code styleing

--- src/main/resources/META-INF/resources/firstswim/javascript/controller/utils/templates.js
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/utils/templates.js
@@ -1,15 +1,15 @@
 /**
  * Register helper to show content of a rdf source
+ * @param {String} url the url of template details
  */
-
 Handlebars.registerHelper('getRDF', function(url) {
     var id = _.uniqueId('getrdf_');
     var requestURL = CONSTANTS.DETAILS_URL + '?iri=' + url;
     var store = rdfstore.create();
     store.load('remote', requestURL, function(success) {
         if(success){
-            var answer = getRDFPropertyValue(store, 'http://www.w3.org/2000/01/rdf-schema#label')
-            $("#"+id).html(answer)
+            var answer = getRDFPropertyValue(store, 'http://www.w3.org/2000/01/rdf-schema#label');
+            $("#"+id).html(answer);
             return "<p id=\""+id+"\">"+ " ... " +"</p>";
         }
     });
