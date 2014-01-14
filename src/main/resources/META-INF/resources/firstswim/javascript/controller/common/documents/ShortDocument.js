aa44b5473917ee84093b7a2cb2687f06fb958325
Stephane Gamard <stephane.gamard@searchbox.com>
2014-01-14 14:44:32 +0100

    Removed abusive log to console

--- src/main/resources/META-INF/resources/firstswim/javascript/controller/common/documents/ShortDocument.js
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/common/documents/ShortDocument.js
@@ -139,7 +139,6 @@ enyo.kind(
      * @param {Object} inEvent the event which is created (inEvent is important in the desktop version)
      */
     openDoc: function(inSender, inEvent){
-      console.log(this)
         this.$.documentMenu.hide();
         this.owner[this.parentFunction](this.url, this.type, inEvent);
     },
