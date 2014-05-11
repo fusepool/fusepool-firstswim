2642221463f73612413fd67fad35f891bcc0a218
Daniël van Adrichem <daniel@treparel.com>
2014-05-11 21:07:06 +0200

    fix check for FusePool.Landscape object, pass enyo object to Landscaping

--- src/main/resources/META-INF/resources/firstswim/javascript/controller/common/landscape/Landscape.js
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/common/landscape/Landscape.js
@@ -10,7 +10,8 @@ enyo.kind(
     published: {
         titleClass: '',
         titleContent: '',
-        loaderClass: ''
+        loaderClass: '',
+        searchWord: ''
     },
 
     /**
@@ -35,8 +36,8 @@ enyo.kind(
      * This function runs, when the user starts a search. It only shows the loader yet.
      */
     startLoading: function(){
-        this.$.loader.show();
-	},
+//        this.$.loader.show();
+    },
 	
 	rendered: function() {
 		this.inherited(arguments); // important! must call the inherited method
@@ -48,12 +49,12 @@ enyo.kind(
 			// first remove the current content
 			this.$.landscapeDiv.setContent('');
 			// then initialize landscaping (if available)
-			if (FusePool === undefined &&
-				FusePool.Landscaping === undefined) {
+			if (window['FusePool'] === undefined ||
+				FusePool['Landscaping'] === undefined) {
 				this.$.landscapeDiv.setContent('Landscape currently unavailable.');
 			}
 			else {
-				FusePool.Landscaping.initialize('.landscapeDiv');
+				FusePool.Landscaping.initialize('.landscapeDiv', this);
 			}
 		}
 	}
