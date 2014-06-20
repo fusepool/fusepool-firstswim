65f00e54363c12044e44bec814d5301f648b2d74
Luca Szabó <szabo.luca@geox.hu>
2014-06-20 13:48:32 +0200

    stylesheet picker preparations

---
+++ src/main/resources/META-INF/resources/firstswim/javascript/controller/desktop/StyleSettingsPopup.js
@@ -0,0 +1,68 @@
+/**
+* @class StyleSettingsPopup
+*/
+enyo.kind(
+/** @lends StyleSettingsPopup.prototype */
+{
+    kind: enyo.Control,
+    name: 'StyleSettingsPopup',
+    classes: 'styleSettingsPopup',
+
+    published: {
+    },
+
+    create: function(){
+        this.inherited(arguments);
+        this.close();
+    },
+  
+    components: [
+        { name: 'popup', classes: 'styleSettingsPopupDiv', kind: onyx.Popup, onHide: 'close', components: [
+            { tag: 'div', classes: 'styleSettingsButtons', components: [
+				{ kind: 'onyx.RadioGroup', name: 'styleSettingsRadio', onActivate: 'setCss', components: [
+					{ content: 'Default', name: 'firstswimBtn', classes: 'styleSettingsButton', value: 'firstswim' },
+					{ content: 'High contrast', name: 'contrastBtn', classes: 'styleSettingsButton', value: 'contrast' }
+				]},				
+            ]}
+        ]},
+        { name: 'closeButton', ontap: 'close', classes: 'styleSettingsCloseButton' }
+    ],
+	
+	setCss: function(inSender, inEvent) {
+		if (inEvent.originator.getActive()) {
+			 var cssName = inSender.getActive().value;
+			$("#mainCss").attr("href", CONSTANTS.STYLE_PATH + cssName + ".css");
+			createCookie('css', cssName, 30);
+		}
+	},
+	
+	setRadioActive: function(cssName) {
+		switch(cssName) {
+			case 'firstswim':
+			case 'contrast':
+				this.$[cssName+'Btn'].setActive(true);
+			break;
+			default:
+				this.$.firstswimBtn.setActive(true);
+		}
+	},
+	
+    /**
+     * This function shows the whole popup.
+     */
+    showStyleSettings: function(){
+        this.$.popup.show();
+        this.$.closeButton.show();
+        this.show();
+    },
+
+    /**
+     * This function runs when the user clicks out of the popup or to the close
+     * button. It hides the whole popup.
+     */
+    close: function(){
+        this.hide();
+        // this.$.closeButton.hide();
+    }    
+
+});
