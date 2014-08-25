eb4c2dcf38e2504cb76af0a651f5cc0c61a15da8
Luca Szabó <szabo.luca@geox.hu>
2014-08-25 10:54:56 +0200

    Full documentation of JS source
    It was generated using YUIDoc (http://yui.github.io/yuidoc/) 2014-08-05

---
+++ doc/api.js
@@ -0,0 +1,46 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AutoSuggest",
+        "Bookmark",
+        "ClosablePopup",
+        "DetailsBox",
+        "Dictionary",
+        "DictionaryController",
+        "DictionaryEntity",
+        "DocumentApp",
+        "DocumentList",
+        "DocumentMobileApp",
+        "DynamicMenu",
+        "LabelItem",
+        "LabelList",
+        "Landscape",
+        "LeftHeader",
+        "LeftPanel",
+        "LocationViewer",
+        "LoginPopup",
+        "MenuController",
+        "MiddlePanel",
+        "NGraph",
+        "OpenedDoc",
+        "OpenedDocHeader",
+        "PredicateAnnotator",
+        "PredicateItem",
+        "PreviewBox",
+        "RightPanel",
+        "SearchBox",
+        "ShortDocument",
+        "TopMessageBox"
+    ],
+    "modules": [
+        "Firstswim"
+    ],
+    "allModules": [
+        {
+            "displayName": "Firstswim",
+            "name": "Firstswim",
+            "description": "Firstswim mobile &amp; desktop application - to making it easy to browse data stored in the Fusepool platform."
+        }
+    ]
+} };
+});
